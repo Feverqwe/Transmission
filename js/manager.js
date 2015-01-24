2a96791c0d737d25a3f8fe780db47821e20af0a3
Anton <anton9121@gmail.com>
2015-01-24 21:39:46 +0500

    1.3.2

--- js/manager.js
+++ js/manager.js
@@ -289,11 +289,7 @@ var manager = {
                             ['drop', manager.onDrop]
                         ],
                         append: [
-                            (key === 'checkbox') ? mono.create('div', {
-                                append: mono.create('input', {
-                                    type: 'checkbox'
-                                })
-                            }) : mono.create('div', {
+                            mono.create('div', {
                                 text: manager.language[value.lang+'_SHORT'] || manager.language[value.lang]
                             }),
                             resizeEl,
@@ -3794,7 +3790,6 @@ var manager = {
                     } else {
                         el.parentNode.parentNode.classList.remove("selected");
                     }
-                    manager.selectAllCheckBox('tr');
                 });
 
                 manager.domCache.flBody.addEventListener('change', function(e) {
@@ -3807,26 +3802,6 @@ var manager = {
                         el.parentNode.parentNode.classList.remove("selected");
                     }
                     manager.selectAllCheckBox('fl');
-                });
-
-                manager.domCache.trFixedHead.addEventListener('change', function(e) {
-                    var el = e.target;
-                    if (el.tagName !== 'INPUT') return;
-
-                    var checkBoxList;
-                    if (el.checked) {
-                        checkBoxList = manager.getCheckBoxList('tr', 0, 1);
-                        for (var i = 0, item; item = checkBoxList[i]; i++) {
-                            item.checked = true;
-                            item.parentNode.parentNode.classList.add("selected");
-                        }
-                    } else {
-                        checkBoxList = manager.getCheckBoxList('tr', 1, 1);
-                        for (var i = 0, item; item = checkBoxList[i]; i++) {
-                            item.checked = false;
-                            item.parentNode.parentNode.classList.remove("selected");
-                        }
-                    }
                 });
 
                 manager.domCache.flFixedHead.addEventListener('change', function(e) {
