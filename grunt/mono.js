53b180aa1345bd0e1cbfb2601b4dc468e5d3ecbc
Anton <anton9121@gmail.com>
2015-05-16 11:49:41 +0500

    1

--- grunt/mono.js
+++ grunt/mono.js
@@ -2,28 +2,14 @@ exports.run = function (grunt) {
     grunt.registerTask('mono', function () {
         var browser = grunt.config('browser');
 
-        var monoPath = './node_modules/mono/src/';
 
-        var mono = grunt.file.read(monoPath + 'mono.js');
-
-        var insertPosition = mono.indexOf('//@');
-        var content = [mono.substr(0, insertPosition), mono.substr(insertPosition)];
-
-        var monoVendorPath = monoPath + 'vendor/';
-        var monoVendor;
-        if (browser === 'firefox') {
-            monoVendor = 'Firefox/';
-        } else {
-            monoVendor = 'Chrome/';
-            content.splice(1, 0, grunt.file.read(monoVendorPath + 'Old' + monoVendor + 'messages.js'));
+        var monoModule = require('mono');
+        if (browser !== 'firefox') {
+            browser = 'oldChrome';
         }
 
-        content.splice(1, 0, grunt.file.read(monoVendorPath + monoVendor + 'storage.js'));
-        content.splice(1, 0, grunt.file.read(monoVendorPath + monoVendor + 'messages.js'));
+        var content = monoModule.get.mono(browser);
 
-        mono = content.join('\n');
-        mono = mono.replace(/^\/\/.*\r?\n/gm, '');
-
-        grunt.file.write(grunt.template.process('<%= output %><%= vendor %><%= dataJsFolder %>mono.js'), mono);
+        grunt.file.write(grunt.template.process('<%= output %><%= vendor %><%= dataJsFolder %>mono.js'), content);
     });
 };
